99c75f569504e1b9a51a674f455008caf866a438
George Hickman <george@ghickman.co.uk>
2009-05-22 21:20:59 +0100

    added logging instead of stdout prints and two test files

--- tvrenamr.py
+++ tvrenamr.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 import urllib
@@ -6,11 +7,17 @@ import xml.etree.ElementTree as ET
 apikey = 'C4C424B4E9137AFD'
 url = "http://www.thetvdb.com/api/"
 working_dir = "/Users/madnashua/Projects/TvRenamr/test"
-named_dir = ""
+renamed_dir = ""
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='/Users/madnashua/Projects/TvRenamr/tvrenamr.log',
+                    filemode='a')
 
 def getInfo(fn):
     m = re.compile("[Ss](\d{2})[Ee](\d{2})").split(fn)
-    if m:
+    if m and len(m) > 1:
         v = m[0].split(".")
         series = ""
         for s in v:
@@ -20,6 +27,10 @@ def getInfo(fn):
         episode = m[2]
         info = [series,season,episode]
         return info
+    else:
+        logging.warning('Incorrect format for auto-renaming: %s', fn)
+        #print "File not in correct format: " + fn
+        return None
 
 def getSeriesInfo(series_name):
     get_series = "GetSeries.php?seriesname="
@@ -30,14 +41,15 @@ def getSeriesInfo(series_name):
         if s == series_name:
             return series.find("seriesid").text
 
-def getEpisodeName(series_id, season, episode):
+def getEpisodeName(series_id, series_name, season, episode):
     episode_url = url+apikey+"/series/"+series_id+"/default/"+season+"/"+episode+"/en.xml"
     f = urllib.urlopen(episode_url)
     try:
         dom = ET.fromstring(f.read())
         return dom.find("Episode").find("EpisodeName").text
     except Exception:
-        print "Episode not found: " + season + episode
+        logging.warning('Episode not found: %s', series_name + " - " + season + episode)
+        #print "Episode not found: " + season + episode
         return None
     
 
@@ -45,17 +57,18 @@ for fn in os.listdir(working_dir):
     extension = fn[-4:]
     if extension == ".avi" or extension == ".mkv":
         file_info = getInfo(fn)
-        #print file_info
+        if file_info == None:
+            continue
         series_id = getSeriesInfo(file_info[0])
-        #print series_id
-        episode = getEpisodeName(series_id, file_info[1], str(int(file_info[2])))
-        #print episode
+        episode = getEpisodeName(series_id, file_info[0], file_info[1], str(int(file_info[2])))
         if episode == None:
             continue
         
-        
         #build filename
         new_fn = file_info[0] + " - " + file_info[1] + file_info[2] + " - " + episode + extension
+        
+        """"
+        NOT CURRENTLY REQUIRED
         
         #build new directory
         new_dir = file_info[0] + "/Season " + str(int(file_info[1])) + "/"
@@ -64,15 +77,21 @@ for fn in os.listdir(working_dir):
         except OSError:
             #print "doesn't exist!"
             os.makedirs(os.path.join(working_dir + "/named", new_dir), 0755)
-            
-        #print new_dir
+        
         if (os.path.exists(working_dir+"/named"+new_dir+new_fn) == False):
             os.rename(os.path.join(working_dir, fn), os.path.join(working_dir + "/named/" +new_dir, new_fn))
             print "Renamed: " + new_fn
         else:
             print "file exists"
+        """
         
-        
+        if os.path.exists(working_dir+new_fn) == False:
+            os.rename(os.path.join(working_dir, fn), os.path.join(working_dir, new_fn))
+            logging.info('Renamed: %s', new_fn)
+            #print "Renamed: " + new_fn
+        else:
+            logging.error('File Exists: %s from %s', new_fn, fn)
+            #print "File Exists"
         
         
         
