65ae1c4b3b9de3e515d7e0fe905760be7cb3f59f
George Hickman <george@ghickman.co.uk>
2009-05-26 22:32:17 +0100

    added exception handling and changed method calls to reflect core changes

--- tvrenamr.py
+++ tvrenamr.py
@@ -6,21 +6,32 @@ parser = OptionParser()
 parser.add_option("-w", "--working", dest="working_dir", help="The working DIRECTORY to run tvrenamr in. Required!")
 parser.add_option("-r", "--renamed", dest="renamed_dir", help="The DIRECTORY to move renamed files to. Mutually exclusive to -a")
 parser.add_option("-a", "--auto", dest="auto_move", help="Automatically move renamed files to the appropriate directory under the DIRECTORY specified. Mutually exclusive to -r")
+parser.add_option("-l", "--log", dest="is_logging", help="")
 (options, args) = parser.parse_args()
 
 working_dir = options.working_dir
 renamed_dir = options.renamed_dir
 auto_move = options.auto_move
+logging = options.is_logging
 
 tv = TvRenamr(working_dir)
 for fn in os.listdir(working_dir):
     if auto_move != None:
-        print "rename and auto move"
-        tv.rename_and_auto_move(fn, auto_move)
+        #rename and auto move
+        try:
+            tv.rename(fn, auto_move=auto_move)
+        except Exception, e:
+            print e
+            continue
     elif renamed_dir != None:
-        print "rename and move"
-        tv.rename_and_move(fn, renamed_dir)
+        #rename and move
+        try:
+            tv.rename(fn, renamed_dir)
+        except Exception, e:
+            print e
+            continue
     elif auto_move == None and renamed_dir == None:
+        #rename
         try:
             tv.rename(fn)
         except Exception, e:
