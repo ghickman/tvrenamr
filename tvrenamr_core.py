6c34de6dd998a42c454e6b7b022b568fb9a12587
George Hickman <george@ghickman.co.uk>
2009-06-10 16:16:41 +0100

    cleaned up imports, added custom exceptions and cleaned up code specifically in extract_file_info

--- tvrenamr_core.py
+++ tvrenamr_core.py
@@ -1,10 +1,10 @@
 import logging
-from optparse import OptionParser
 import os
 import re
-from thetvdb import TheTvDb
-from tvrage import TvRage
 import sys
+
+from errors import *
+from thetvdb import TheTvDb
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -21,17 +21,17 @@ class TvRenamr():
         self.logging = logging
     
     def __extract_file_info(self, fn, user_regex=None):
-        if re.compile(".*?\s-\s[\d]{3,4}\s-\s.*?\."+fn[-3:]).match(fn): raise Exception('Already in correct naming format: '+ fn)
-        fn = fn.replace("_", ".")
-        fn = fn.replace(" ", ".")
+        if re.compile(".*?\s-\s[\d]{3,4}\s-\s.*?\."+fn[-3:]).match(fn): raise AlreadyNamedException(fn)
+        fn = fn.replace("_", ".").replace(" ", ".")
         if user_regex == None: regex = "(?P<series>[\w\s._]+)\.[Ss]?(?P<season>[\d]{1,2}?)([Xx]|[Ee])(?P<episode>[\d]{1,2})"
         else: regex = user_regex.replace('%n', '(?P<series>[\w\s._]+)').replace('%s', "(?P<season>[0-9]{1,2}?)").replace('%e', '(?P<episode>[0-9]{1,2})')
         m = re.compile(regex).match(fn)
-        series = m.group('series').replace('.',' ')
-        if series.find('The O C') >= 0: series = series.replace('The O C','The O.C.')
-        if re.compile('[\d]{4}').match(series[-4:]) != None: series = "%s(%s)" % (series[:-4], series[-4:])
-        if m != None: return [series,str(int(m.group('season'))),m.group('episode'),fn[-4:]]
-        else: raise Exception('Skipped due to unexpected format: '+ fn)
+        if m != None:
+            series = m.group('series').replace('.',' ')
+            if series.find('The O C') >= 0: series = series.replace('The O C','The O.C.')
+            if re.compile('\s{1}[\d]{4}').match(series[-5:]) != None: series = "%s(%s)" % (series[:-4], series[-4:])
+            return [series,str(int(m.group('season'))),m.group('episode'),fn[-4:]]
+        else: raise UnexpectedFormatException(fn)
     
     def __build_file_name(self, fn):
         t = TheTvDb(fn[0])
@@ -52,19 +52,16 @@ class TvRenamr():
         else: raise Exception('File Exists: '+ new_fn +' from: '+fn)
     
     def rename(self, fn, regex=None, renamed_dir=None, auto_move=False):
-        """"
-        
+        """
         """
         #stage
-        try:
-            f = self.__extract_file_info(fn,regex)
+        try: f = self.__extract_file_info(fn,regex)
         except Exception, e: raise
         new_fn = self.__build_file_name(f)
         
         #build new path
         if renamed_dir == None: renamed_dir = self.working_dir
-        if auto_move != False: 
-            dest_path = self.__build_auto_path(new_fn, f[0], f[1], renamed_dir, auto_move)
+        if auto_move != False: dest_path = self.__build_auto_path(new_fn, f[0], f[1], renamed_dir, auto_move)
         else: dest_path = os.path.join(renamed_dir, new_fn)
         
         #rename
