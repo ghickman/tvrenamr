ffc2982a76c374bf108ad1fb77fbb7cfda745283
George Hickman <george@ghickman.co.uk>
2009-06-10 16:12:31 +0100

    added options to deal with show name and logging

--- tvrenamr.py
+++ tvrenamr.py
@@ -4,7 +4,8 @@ import os
 
 parser = OptionParser()
 parser.add_option("-a", "--auto", dest="auto_move", help="Automatically move renamed files to the appropriate directory under the directory specified. Mutually exclusive to -r")
-parser.add_option("-l", "--log", dest="is_logging", help="")
+parser.add_option("-l", "--log", dest="logging", help="Set the location for logs to be created in")
+parser.add_option("-n", "--name", dest="name", help="Set the show name for renaming")
 parser.add_option("-r", "--renamed", dest="renamed_dir", help="The directory to move renamed files to. Mutually exclusive to -a")
 parser.add_option("--regex", dest="regex", help="the regular expression to set the format of files being renamed. Use %n to specify the show name, %s for the season number and %e for the episode number. All spaces are converted to periods before the regex is run")
 parser.add_option("-w", "--working", dest="working_dir", help="The working directory to run tvrenamr in. Required!")
@@ -13,9 +14,10 @@ parser.add_option("-w", "--working", dest="working_dir", help="The working direc
 working_dir = options.working_dir
 renamed_dir = options.renamed_dir
 auto_move = options.auto_move
-logging = options.is_logging
+logging = options.logging
 regex = options.regex
-#working_dir = "/Users/madnashua/Projects/tvrenamr/core/OC"
+#working_dir = "/Users/madnashua/Projects/tvrenamr/core/test"
+#regex = "%n\.\-\.[Ss]?%s\.E%e"
 
 tv = TvRenamr(working_dir)
 for fn in os.listdir(working_dir):
