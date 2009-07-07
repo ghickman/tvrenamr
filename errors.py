bdcd85a339b3fd34d5dbc76aa73f3175ea1351ca
George Hickman <george@ghickman.co.uk>
2009-07-07 15:06:41 +0100

    cleaned up code and added episode already exists in folder exception

--- errors.py
+++ errors.py
@@ -1,21 +1,28 @@
 class AlreadyNamedException(Exception):
     """
     """
-    def __init__(self, fn):
+    def __init__(self,fn):
         msg = "Already in correct naming format: "+fn
-        
         print msg
     
 class UnexpectedFormatException(Exception):
     """
     """
-    def __init__(self, fn):
+    def __init__(self,fn):
         msg = "The file was in an unexpected format: "+fn
         print msg
     
 class EpisodeNotFoundException(Exception):
     """
     """
-    def __init__(self, fn):
+    def __init__(self,fn):
         msg = "Episode could not be found: "+fn
         print msg
+    
+class EpisodeAlreadyExistsInFolderException(Exception):
+    """
+    """
+    def __init__(self,fn,new_fn):
+        msg = "An episode called \'"+fn+"\' already exists in the current folder"
+        print msg
+    
