6e7e8d1f6047314d1e2b34f1400fccdf4391b448
George Hickman <george@ghickman.co.uk>
2009-07-16 02:48:55 +0100

    added tests for thetvdb

--- tests/test_thetvdb.py
+++ tests/test_thetvdb.py
@@ -13,8 +13,10 @@ class TestTheTvDb(object):
         self.tv = TvRenamr("tests/data/files/")
     
     def test_searching_the_tv_db_with_an_ambiguous_name_returns_the_correct_show(self):
-        details = self.tv.extract_episode_details_from_file('chuck.s02e05.avi')
-        name = self.tv.retrieve_episode_name(details[0],details[1],details[2])
-        path = self.tv.build_path(details,name)
-        assert_raises(EpisodeAlreadyExistsInFolderException, self.tv.rename, 'chuck.s02e05.avi', path)
+        assert_equal(self.tv.retrieve_episode_name('the o.c.','03','04'), 'The Last Waltz')
     
+    def test_searching_the_tv_db_for_an_incorrect_name_returns_a_show_not_found_exception(self):
+        assert_raises(ShowNotFoundException, self.tv.retrieve_episode_name, 'west wing', '04', '01')
+    
+    def test_searching_the_tv_db_for_an_episode_that_does_not_exist_returns_an_episode_not_found_exception(self):
+        assert_raises(EpisodeNotFoundException, self.tv.retrieve_episode_name, 'chuck', '03', '32')
