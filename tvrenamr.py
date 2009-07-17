8008e53feac09fcf24e75e399b7b0e664d9eed59
George Hickman <george@ghickman.co.uk>
2009-07-17 22:10:11 +0100

    fixed recursively looking through folders and caught exceptions when calling api

--- tvrenamr.py
+++ tvrenamr.py
@@ -15,15 +15,17 @@ parser.add_option("-w", "--working", dest="working_dir", help="The working direc
 #options.working_dir = "/Users/madnashua/Projects/tvrenamr/core/testcase/scrubs.s03e05.avi"
 def script_rename(working_dir, fn):
     tv = TvRenamr(working_dir)
-    details = tv.extract_episode_details_from_file(fn, user_regex=options.regex)
-    name = tv.retrieve_episode_name(details[0],details[1],details[2])
-    path = tv.build_path(details, name, renamed_dir=options.renamed_dir, auto_move=options.auto_move)
-    tv.rename(fn,path)
+    try:
+        details = tv.extract_episode_details_from_file(fn, user_regex=options.regex)
+        name = tv.retrieve_episode_name(details[0],details[1],details[2])
+        path = tv.build_path(details, name, renamed_dir=options.renamed_dir, auto_move=options.auto_move)
+        tv.rename(fn,path)
+    except Exception, e: print e
 
 if os.path.isdir(options.working_dir):
     for each_tuple in os.walk(options.working_dir):
         for fname in each_tuple[2]:
-            script_rename(options.working_dir, os.path.abspath(os.path.join(each_tuple[0], fname)))
+            script_rename(each_tuple[0], fname)
 elif os.path.isfile(options.working_dir):
     working = os.path.split(options.working_dir)
     script_rename(working[0], working[1])
