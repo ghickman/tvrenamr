977b71b54b38e52e97f6c1a28d6fe50d6c523315
George Hickman <george@ghickman.co.uk>
2009-08-02 22:38:03 +0100

    added clean_names method, which will allow the user to clean the series and episode name of an episode before renaming the file fixed a spelling mistake in the build_path docstring changed the regex in extract_episode_details_from_file to add the format series.000.xxx to the list of default file formats and updated its docstring

--- tvrenamr_core.py
+++ tvrenamr_core.py
@@ -11,13 +11,15 @@ class TvRenamr():
     def extract_episode_details_from_file(self, fn, user_regex=None):
         """
         Looks at the file given and extracts from it the show title, it's season number and episode number using regular expression ninja skills. The 
-        default formats accepted are: series.0x00.xxx or series.s0e00.xxx
+        default formats accepted are: series.0x00.xxx or series.s0e00.xxx or series.000.xxx
         A user can specify their own regular expression for a format not already covered.
         """
-        if re.compile(".*?\s-\s[\d]{3,4}\s-\s.+?\."+fn[-3:]).match(fn): raise AlreadyNamedException(fn)
+        #if re.compile(".*?\s-\s[\d]{3,4}\s-\s.+?\."+fn[-3:]).match(fn): raise AlreadyNamedException(fn)
         fn = fn.replace("_", ".").replace(" ", ".")
-        if user_regex == None: regex = "(?P<series>[\w\s._]+)\.[Ss]?(?P<season>[\d]{1,2}?)([Xx]|[Ee])(?P<episode>[\d]{1,2})"
-        else: regex = user_regex.replace('%n', '(?P<series>[\w\s._]+)').replace('%s', "(?P<season>[0-9]{1,2}?)").replace('%e', '(?P<episode>[0-9]{1,2})')
+        print fn
+        if user_regex == None: regex = "(?P<series>[\w\s._-]+)\.[Ss]?(?P<season>[\d]{1,2})([Xx]|[Ee]|)(?P<episode>[\d]{2})"
+        else: regex = user_regex.replace('%n', '(?P<series>[\w\s._-]+)').replace('%s', "(?P<season>[0-9]{1,2})").replace('%e', '(?P<episode>[0-9]{2})')
+        print regex 
         m = re.compile(regex).match(fn)
         if m != None:
             series = m.group('series').replace('.',' ')
@@ -37,7 +39,7 @@ class TvRenamr():
     
     def build_path(self, details, series_name, episode_name, renamed_dir=None, auto_move=None):
         """
-        Builds the new path for the file to be renamed to, by default this is the working directory. Users can specify a directoy to move files to 
+        Builds the new path for the file to be renamed to, by default this is the working directory. Users can specify a directory to move files to 
         once renamed using the renamed_dir option. The auto_move option can be used to specify a top level directory where files will be placed in
         season and series folders, i.e. series -> season 1 -> episodes
         """
@@ -46,6 +48,14 @@ class TvRenamr():
         if auto_move != None: renamed_dir = auto_move + details[0] +"/Season "+ str(int(details[1])) +"/"+ new_fn
         elif renamed_dir == None: renamed_dir = self.working_dir
         return os.path.join(renamed_dir, new_fn)
+    
+    def clean_names(self, fn):
+        """
+        Cleans the filename passed in, making it be safe for all file systems. Also allows the user to specify the new characters to be used.
+        """
+        print fn
+        fn.replace(':',' -')
+        return fn
     
     def rename(self, fn, new_fn):
         """
