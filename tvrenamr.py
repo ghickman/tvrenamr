a764f654f35d1b2f1e2f032e7c9fe24cac10a84c
George Hickman <george@ghickman.co.uk>
2009-08-04 16:32:31 +0100

    replaced double quotes with single quotes to maintain consistency and added the ability to specify season and episode numbers through the script

--- tvrenamr.py
+++ tvrenamr.py
@@ -4,12 +4,13 @@ from optparse import OptionParser
 from tvrenamr_core import TvRenamr
 
 parser = OptionParser()
-parser.add_option("-a", "--auto", dest="auto_move", help="Automatically move renamed files to the appropriate directory under the directory specified. Mutually exclusive to -r")
-parser.add_option("-n", "--name", dest="name", help="Set the show name for renaming")
-parser.add_option("-r", "--renamed", dest="renamed_dir", help="The directory to move renamed files to. Mutually exclusive to -a")
-parser.add_option("--regex", dest="regex", help="the regular expression to set the format of files being renamed. Use %n to specify the show name, %s for the season number and %e for the episode number. All spaces are converted to periods before the regex is run")
-parser.add_option("-s", "--season", dest="season")
-parser.add_option("-w", "--working", dest="working_dir", help="The working directory to run tvrenamr in. Required!")
+parser.add_option('-a', '--auto', dest='auto_move', help='Automatically move renamed files to the appropriate directory under the directory specified. Mutually exclusive to -r')
+parser.add_option('-e', '--episode', dest='episode', help='Set the episode number. Currently this will cause errors when working with more than one file')
+parser.add_option('-n', '--name', dest='name', help='Set the show name for renaming')
+parser.add_option('-r', "--renamed", dest='renamed_dir', help='The directory to move renamed files to. Mutually exclusive to -a')
+parser.add_option('--regex', dest='regex', help='The regular expression to set the format of files being renamed. Use %n to specify the show name, %s for the season number and %e for the episode number. All spaces are converted to periods before the regex is run')
+parser.add_option('-s', '--season', dest='season', help='Set the season number.')
+parser.add_option('-w', '--working', dest='working_dir', help='The working directory to run tvrenamr in. Required!')
 (options, args) = parser.parse_args()
 
 #options.working_dir = ''
@@ -22,8 +23,11 @@ def script_rename(working_dir, fn):
         details = tv.extract_episode_details_from_file(fn, user_regex=options.regex)
         print details
         if options.name: details[0]=options.name
+        if options.season: details[1]=options.season
+        if options.episode: details[2]=options.episode
         names = tv.retrieve_episode_name(details[0],details[1],details[2])
         path = tv.build_path(details, series_name=names[0], episode_name=names[1], renamed_dir=options.renamed_dir, auto_move=options.auto_move)
+        print path
         tv.rename(fn,path)
     except Exception, e: print e
 
