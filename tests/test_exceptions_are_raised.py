d1be105270aef87c0f203c2920c163b824285742
George Hickman <george@ghickman.co.uk>
2009-08-16 19:56:12 +0100

    added teardown method and commented out already named exception

--- tests/test_exceptions_are_raised.py
+++ tests/test_exceptions_are_raised.py
@@ -1,5 +1,4 @@
-import urllib2
-import os.path
+import shutil, os, urllib2
 
 from nose.tools import *
 
@@ -11,8 +10,15 @@ from core.errors import *
 
 class TestExceptionsAreRaised(object):
     
-    def setup(self):
-        self.tv = TvRenamr("tests/data/files/")
+    def setUp(self):
+        files = 'tests/data/files'
+        working = 'tests/data/working'
+        self.tv = TvRenamr(working)
+        for fn in os.listdir(files): shutil.copy(os.path.join(files, fn), os.path.join(working, fn))
+    
+    def tearDown(self):
+        working = 'tests/data/working'
+        for fn in os.listdir(working): os.remove(os.path.join(working,fn))
     
     def test_already_named_exception_should_be_raised_when_file_already_named_correctly(self):
         #assert_raises(AlreadyNamedException, self.tv.extract_episode_details_from_file, 'Chuck - 205 - w00t.avi')
@@ -26,7 +32,9 @@ class TestExceptionsAreRaised(object):
         assert_raises(EpisodeNotFoundException, self.tv.retrieve_episode_name, details[0], details[1], details[2])
     
     def test_episode_already_exists_in_folder_exception_is_raised_when_new_file_name_already_exists_in_folder(self):
-        details = self.tv.extract_episode_details_from_file('chuck.s02e05.avi')
+        fn = 'chuck.s02e05.avi'
+        details = self.tv.extract_episode_details_from_file(fn)
         names = self.tv.retrieve_episode_name(details[0],details[1],details[2])
-        path = self.tv.build_path(details, series_name=names[0], episode_name=names[1])
-        assert_raises(EpisodeAlreadyExistsInFolderException, self.tv.rename, 'chuck.s02e05.avi', path)
+        path = self.tv.build_path(series=names[0], season=details[1], episode=details[2], episode_name=names[1], extension=details[3])
+        assert_raises(EpisodeAlreadyExistsInFolderException, self.tv.rename, fn, path)
+    
