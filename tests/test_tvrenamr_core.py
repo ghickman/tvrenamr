8e3ea6e2ec8aae56606e3c3c316111412688a536
George Hickman <george@ghickman.co.uk>
2009-10-01 13:30:52 +0100

    created a class level working directory definition and changed the test cases to respect this. Moved one testcase to the auto_move class where it was better suited.

--- tests/test_tvrenamr_core.py
+++ tests/test_tvrenamr_core.py
@@ -8,16 +8,15 @@ import urlopenmock
 from core.tvrenamr_core import TvRenamr
 
 class TestTvrenamrCore(object):
+    working = 'tests/data/working'
     
     def setUp(self):
         files = 'tests/data/files'
-        working = 'tests/data/working'
-        self.tv = TvRenamr(working)
-        for fn in os.listdir(files): shutil.copy(os.path.join(files, fn), os.path.join(working, fn))
+        self.tv = TvRenamr(self.working)
+        for fn in os.listdir(files): shutil.copy(os.path.join(files, fn), os.path.join(self.working, fn))
     
     def tearDown(self):
-        working = 'tests/data/working'
-        for fn in os.listdir(working): os.remove(os.path.join(working,fn))
+        for fn in os.listdir(self.working): os.remove(os.path.join(self.working,fn))
     
     def test_instantiate_core(self):
         assert_true(isinstance(TvRenamr("/"), TvRenamr))
@@ -27,7 +26,8 @@ class TestTvrenamrCore(object):
         details = self.tv.extract_episode_details_from_file(fn)
         names = self.tv.retrieve_episode_name(series='Avatar: The Last Airbender', season=details[1], episode=details[2])
         path = self.tv.build_path(series=names[0], season=details[1], episode=details[2], episode_name=names[1], extension=details[3])
-        assert_equal(self.tv.rename(fn, path), 'Avatar: The Last Airbender - 108 - Winter Solstice (2): Avatar Roku.avi')
+        self.tv.rename(fn, path)
+        assert_true(os.path.isfile(os.path.join(self.working, 'Avatar: The Last Airbender - 108 - Winter Solstice (Part 2): Avatar Roku.avi')))
     
     def test_passing_in_a_season_number_to_retrieve_episode_name_returns_the_correct_episode_name_from_that_season(self):
         details = self.tv.extract_episode_details_from_file('chuck.s1e08.blah.avi')
@@ -39,16 +39,20 @@ class TestTvrenamrCore(object):
         details = self.tv.extract_episode_details_from_file(fn)
         names = self.tv.retrieve_episode_name(series=details[0], season='2', episode=details[2])
         path = self.tv.build_path(series=names[0], season='2', episode=details[2], episode_name=names[1], extension=details[3])
-        assert_equal(self.tv.rename(fn, path), 'Chuck - 208 - Chuck Versus the Gravitron.avi')
+        self.tv.rename(fn, path)
+        assert_true(os.path.isfile(os.path.join(self.working, 'Chuck - 208 - Chuck Versus the Gravitron.avi')))
     
     def test_passing_in_an_episode_number_renames_a_file_using_that_episode_number(self):
         details = self.tv.extract_episode_details_from_file('chuck.s1e08.blah.HDTV.XViD.avi')
         names = self.tv.retrieve_episode_name(series=details[0], season=details[1], episode='9')
         assert_equal('Chuck Versus the Imported Hard Salami', names[1])
     
-    def test_using_auto_move_returns_the_correct_path_based_on_the_episode(self):
-        details = self.tv.extract_episode_details_from_file('true.blood.0205.avi')
+    def test_setting_the_position_of_the_variable_to_true_places_a_shows_the_at_the_end_of_the_file_name(self):
+        fn = 'The.Big.Bang.Theory.S03E01.HDTV.XviD-NoTV.avi'
+        details = self.tv.extract_episode_details_from_file(fn)
         names = self.tv.retrieve_episode_name(series=details[0], season=details[1], episode=details[2])
-        path = self.tv.build_path(series=names[0], season=details[1], episode=details[2], episode_name=names[1], extension=details[3], auto_move='tests/data/auto_move')
-        assert_equal(path, 'tests/data/auto_move/True Blood/Season 2/True Blood - 205 - Never Let Me Go.avi')
+        name = self.tv.set_position_of_the_to_the_end_of_a_shows_name(names[0])
+        path = self.tv.build_path(series=name, season=details[1], episode=details[2], episode_name=names[1], extension=details[3])
+        self.tv.rename(fn, path)
+        assert_true(os.path.isfile(os.path.join(self.working, 'Big Bang Theory, The - 301 - The Electric Can Opener Fluctuation.avi')))
     
