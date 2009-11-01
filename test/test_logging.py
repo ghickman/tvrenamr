691be09842744382bff3ee4b15caa7da8fc29522
George Hickman <george@ghickman.co.uk>
2009-11-01 16:19:11 +0000

    updated title

--- test/test_logging.py
+++ test/test_logging.py
@@ -19,10 +19,10 @@ class TestLogging(object):
     def test_passing_in_a_series_name_renames_a_file_using_that_name(self):
         fn = 'avatar.s1e08.blah.HDTV.XViD.avi'
         credentials = self.tv.extract_episode_details_from_file(fn, user_regex='%n.s%s{1}e%e{2}.blah')
-        title = self.tv.retrieve_episode_name(series='Avatar: The Last Airbender', season=credentials['season'], episode=credentials['episode'])
-        credentials['series'] = title['series']
-        credentials['title'] = title['title']
+        # title = self.tv.retrieve_episode_name(series='Avatar: The Last Airbender', season=credentials['season'], episode=credentials['episode'])
+        credentials['series'] = 'Avatar: The Last Airbender'
+        credentials['title'] = 'Winter Solstice (2): Avatar Roku'
         path = self.tv.build_path(series=credentials['series'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'])
         self.tv.rename(fn, path)
-        assert_true(os.path.isfile(os.path.join(self.working, 'Avatar: The Last Airbender - 108 - Winter Solstice (2): Avatar Roku.avi')))
+        assert_true(os.path.exists(os.path.join(self.working, 'Avatar: The Last Airbender - 108 - Winter Solstice (2): Avatar Roku.avi')))
     
