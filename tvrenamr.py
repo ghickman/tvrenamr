9eafaa85d96bff5be8772d4fbfb2950efbf400a5
George Hickman <george@ghickman.co.uk>
2009-11-15 18:06:02 +0000

    added logging option and merged folder changes from RichGuk

--- tvrenamr.py
+++ tvrenamr.py
@@ -10,6 +10,8 @@ import core
 parser = OptionParser()
 parser.add_option('-a', '--auto', action='store_true', dest='organise', help='Automatically move renamed files to the directory specified in renamed and organise them appropriated according to their show name and season number')
 parser.add_option('-e', '--episode', dest='episode', help='Set the episode number. Currently this will cause errors when working with more than one file')
+parser.add_option('--ignore-recursive', action='store_true', dest='ignore_recursive', default=False, help='Only use files from the root of a given directory do not enter any sub-directories')
+parser.add_option('-l', '--log_level', dest='log', default='debug', help='Set the log level')
 parser.add_option('-n', '--name', dest='name', help='Set the show name for renaming')
 parser.add_option('-o', '--output', dest='output_format', help='Set the output format for the episodes being renamed')
 parser.add_option('-r', "--renamed", dest='renamed', help='The directory to move renamed files to, if not specified the working directory is used')
@@ -19,11 +21,32 @@ parser.add_option('-t', '--the', action='store_true', dest='the', help='Set the 
 parser.add_option('-x', '--exceptions', dest='exceptions', help='Specify an exceptions file')
 (options, args) = parser.parse_args()
 
-def __determine_type(path):
+def __determine_type(path, ignore_recursive=False, ignore_filelist=None):
+    """
+    Determines which files need to be processed for renaming.
+
+    :param path: The input file or directory.
+    :param ignore_recursive: To ignore a recursive search for files if
+    `path` is a directory. Default is False.
+    :param ignore_filelist: Optional set of files to ignore from renaming.
+    Often used by filtering methods such as Deluge.
+
+    :returns: A list of files to be renamed.
+    :rtype: A list of dictionaries, who's keys are `directory` and `filename`
+    """
     if os.path.isdir(path):
         filelist = []
         for root, dirs, files in os.walk(path):
-            for fname in files: filelist.append({'directory': root, 'filename': fname})
+            for fname in files:
+                # If we have a file we should be ignoring and skipping it.
+                if ignore_filelist is not None and (os.path.join(root, fname) in ignore_filelist):
+                    continue
+
+                filelist.append({'directory': root, 'filename': fname})
+            # Don't want a recusive walk?
+            if ignore_recursive:
+                break
+
         return filelist
     elif os.path.isfile(path):
         working = os.path.split(path)
