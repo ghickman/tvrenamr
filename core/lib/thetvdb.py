ed6a03c80dfa7c11ffcac05cf341a5d2bc86c1e6
George Hickman <george@ghickman.co.uk>
2009-11-17 22:18:48 +0000

    moved url definitions outside of class and refactored code to cope. Updated Exceptions to more specific exceptions for the context

--- core/lib/thetvdb.py
+++ core/lib/thetvdb.py
@@ -6,21 +6,21 @@ from core.errors import *
 
 log = logging.getLogger('The Tv DB')
 
+url_base = 'http://www.thetvdb.com/api/'
+url_series = 'GetSeries.php?seriesname='
+apikey = 'C4C424B4E9137AFD'
+
 class TheTvDb():
-    url = 'http://www.thetvdb.com/api/'
-    apikey = 'C4C424B4E9137AFD'
-    get_series = 'GetSeries.php?seriesname='
-    
     def __init__(self, series_name):
         self.series = series_name
         
     def __get_series_id(self):
-        url = self.url + self.get_series + urllib2.quote(self.series)
+        url = url_base.url + url_series + urllib2.quote(self.series)
         log.debug('Series url: '+url)
         try: data = urllib2.urlopen(url).read()
         except urllib2.URLError: raise
         dom = ET.fromstring(data)
-        if dom is None: raise Exception('Error retriving XML for for: '+ self.series)
+        if dom is None: raise SeriesIdNotFoundException(self.series)
         log.debug('XML retrieved, searching for series')
         for name in dom.findall("Series"):
             s = name.find("SeriesName").text
@@ -34,12 +34,13 @@ class TheTvDb():
         log.debug('Retrieving series id for %s' % self.series)
         series_id = self.__get_series_id()
         log.debug('Building episode url')
-        episode_url = self.url + self.apikey +'/series/'+ series_id +'/default/'+ str(int(season)) +'/'+ str(int(episode)) +'/en.xml'
+        episode_url = url_base + apikey +'/series/'+ series_id +'/default/'+ str(int(season)) +'/'+ str(int(episode)) +'/en.xml'
+        log.debug(episode_url)
         log.debug('Attempting to retrieve episode name')
-        try: f = urllib2.urlopen(episode_url)
+        try: f = urllib2.urlopen(episode_url) # timeout after 15 seconds
         except urllib2.URLError: raise EpisodeNotFoundException(season+episode)
         dom = ET.fromstring(f.read())
-        if dom != None:
+        if dom is not None:
             title = dom.find("Episode").find("EpisodeName").text
             log.info('Retrieved episode: %s' % title)
             return {'series': self.series, 'title': title}
