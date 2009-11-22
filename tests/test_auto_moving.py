410dd6af3265c7da9582f4086163238f8ed216f3
George Hickman <george@ghickman.co.uk>
2009-11-22 19:15:16 +0000

    refactored all series variables to show

--- tests/test_auto_moving.py
+++ tests/test_auto_moving.py
@@ -26,20 +26,20 @@ class TestAutoMoving(object):
     def test_using_organise_renames_the_file_correctly(self):
         fn = 'chuck.s1e06.foo.HD.avi'
         credentials = self.tv.extract_episode_details_from_file(fn)
-        title = self.tv.retrieve_episode_name(series=credentials['series'], season=credentials['season'], episode=credentials['episode'])
-        credentials['series'] = title['series']
+        title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
+        credentials['show'] = title['show']
         credentials['title'] = title['title']
-        path = self.tv.build_path(series=credentials['series'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
+        path = self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
         self.tv.rename(fn, path)
         assert_true(os.path.isfile(os.path.join(self.organised+'/Chuck/Season 1', 'Chuck - 106 - Chuck Versus the Sandworm.avi')))
     
     def test_using_organise_moves_the_file_to_the_correct_folder(self):
         fn = 'stargate.sg-1.s10e18.xvid.avi'
         credentials = self.tv.extract_episode_details_from_file(fn)
-        title = self.tv.retrieve_episode_name(series=credentials['series'], season=credentials['season'], episode=credentials['episode'])
-        credentials['series'] = title['series']
+        title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
+        credentials['show'] = title['show']
         credentials['title'] = title['title']
-        path = self.tv.build_path(series=credentials['series'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
+        path = self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
         self.tv.rename(fn, path)
         for fn in os.listdir(self.organised):
             if fn == 'Stargate SG-1':
@@ -53,19 +53,19 @@ class TestAutoMoving(object):
 
     def test_using_organise_returns_the_correct_path_based_on_the_episode(self):
         credentials = self.tv.extract_episode_details_from_file('true.blood.0205.avi')
-        title = self.tv.retrieve_episode_name(series=credentials['series'], season=credentials['season'], episode=credentials['episode'])
-        credentials['series'] = title['series']
+        title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
+        credentials['show'] = title['show']
         credentials['title'] = title['title']
-        path = self.tv.build_path(series=credentials['series'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
+        path = self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
         assert_equal(path, 'tests/data/organised/True Blood/Season 2/True Blood - 205 - Never Let Me Go.avi')
     
-    def test_moving_the_leading_the_to_the_end_of_a_show_name_causes_the_series_folder_name_to_follow_suit_when_using_organise(self):
+    def test_moving_the_leading_the_to_the_end_of_a_show_name_causes_the_show_folder_name_to_follow_suit_when_using_organise(self):
         fn = 'The.Big.Bang.Theory.S03E01.HDTV.XviD-NoTV.avi'
         credentials = self.tv.extract_episode_details_from_file(fn)
-        title = self.tv.retrieve_episode_name(series=credentials['series'], season=credentials['season'], episode=credentials['episode'])
+        title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
         credentials['title'] = title['title']
-        credentials['series'] = self.tv.set_position_of_leading_the_to_end_of_series_name(title['series'])
-        path = self.tv.build_path(series=credentials['series'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
+        credentials['show'] = self.tv.move_leading_the_to_trailing_the(title['show'])
+        path = self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], organise=self.organise, renamed_dir=self.organised)
         self.tv.rename(fn, path)
         assert_true(os.path.isdir(os.path.join(self.organised, 'Big Bang Theory, The/Season 3')))
     
