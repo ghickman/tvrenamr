78733916b76083d3df14dc3c9cd8c8ec38ce050e
George Hickman <george@ghickman.co.uk>
2009-11-25 07:54:16 +0000

    removed unnecessary code

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -19,7 +19,6 @@ class TestLogging(object):
     def test_passing_in_a_series_name_renames_a_file_using_that_name(self):
         fn = 'avatar.s1e08.blah.HDTV.XViD.avi'
         credentials = self.tv.extract_episode_details_from_file(fn, user_regex='%n.s%s{1}e%e{2}.blah')
-        # title = self.tv.retrieve_episode_name(series='Avatar: The Last Airbender', season=credentials['season'], episode=credentials['episode'])
         credentials['show'] = 'Avatar: The Last Airbender'
         credentials['title'] = 'Winter Solstice (2): Avatar Roku'
         path = self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'])
