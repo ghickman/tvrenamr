8d6f1816c8681e048c11ac604238d92f0fb7e525
George Hickman <george@ghickman.co.uk>
2009-11-28 12:28:59 +0000

    updated base code to follow the new script more closely but modified the leading the and exceptions file sections to no end the script
    cleaned up options and added a few extra ones to follow options in main
    script
    
    updated positions of pid and stdout files to be based upon the script
    location

--- tvrenamrd.py
+++ tvrenamrd.py
@@ -11,41 +11,46 @@ log = logging.getLogger('daemon')
 
 class WatchFolder(ProcessEvent):
     def __init__(self): 
+        pass
     
     def process_IN_MOVED_TO(self, event):
         log.debug('MOVED: '+event.pathname)
         if not event.name.startswith('.'):
             if event.dir:
                 for each_tuple in os.walk(event.pathname):
-                    for fname in each_tuple[2]:
-                        self.__rename([each_tuple[0], fname])
-            else: self.__rename(os.path.split(event.pathname))
+                    for fname in each_tuple[2]: self.__rename(each_tuple[0], fname)
+            else: self.__rename(*os.path.split(event.pathname))
     
     def process_IN_CREATE(self, event):
         log.debug('CREATE: '+event.pathname)
-        if not event.name.startswith('.') and not event.dir: self.__rename(os.path.split(event.pathname))
+        if not event.name.startswith('.') and not event.dir: self.__rename(*os.path.split(event.pathname))
     
-    def __rename(self, pathname):
-        tv = TvRenamr(pathname[0], 'debug')
+    def __rename(self, directory, filename):
         try:
-            credentials = tv.extract_episode_details_from_file(pathname[1])
+            tv = TvRenamr(directory, options.log, options.logfile)
+            credentials = tv.extract_episode_details_from_file(filename)
             if options.exceptions is not None:
-                try: credentials['series'] = tv.convert_show_names_using_exceptions_file(options.exceptions, credentials['series'])
-                except Exception: pass
-            title = tv.retrieve_episode_name(credentials['series'],credentials['season'],credentials['episode'])
-            credentials['series'] = title['series']
+                try: credentials['show'] = tv.convert_show_names_using_exceptions_file(options.exceptions, credentials['show'])
+                except: ShowNotInExceptionsList
+            title = tv.retrieve_episode_name(credentials['show'],credentials['season'],credentials['episode'])
+            credentials['show'] = title['show']
             if options.the:
-                try: credentials['series'] = tv.set_position_of_leading_the_to_end_of_show_name(credentials['series'])
+                try: credentials['series'] = tv.move_leading_the_to_trailing_the(credentials['show'])
                 except NoLeadingTheException: pass
             credentials['title'] = title['title']
-            path = tv.build_path(series=credentials['series'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], renamed_dir=options.renamed, organise=options.organise, format=options.output_format)
-            tv.rename(pathname[1],path)
-        except Exception: pass
-
+            path = tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], renamed_dir=options.renamed, organise=options.organise, format=options.output_format)
+            tv.rename(filename, path)
+        except Exception, e:
+            log.error(e)
+            pass
+    
 if __name__=="__main__":
     parser = OptionParser()
-    parser.add_option('-a', '--auto', action='store_true', dest='organise', help='Automatically move renamed files to the directory specified in renamed and organise them appropriated according to their show name and season number')
+    parser.add_option('-l', '--log_level', dest='log', default='info', help='Set the log level. Valid options are debug, info, warning, error and critical.')
+    parser.add_option('--logfile', dest='logfile', help='Set the location of the log file.')
+    parser.add_option('--library', dest='library', help='Set the library to use for retrieving episode titles. This defaults to tvrage, but thetvdb is also available.')
     parser.add_option('-o', '--output', dest='output_format', help='Set the output format for the episodes being renamed')
+    parser.add_option('--organise', action='store_true', dest='organise', help='Automatically move renamed files to the directory specified with -r and organise them based on their show name and season number.')
     parser.add_option('-r', "--renamed", dest='renamed', help='The directory to move renamed files to, if not specified the working directory is used')
     parser.add_option('-t', '--the', action='store_true', dest='the', help='Set the position of \'The\' in a show\'s name to the end of the file')
     parser.add_option('-x', '--exceptions', dest='exceptions', help='Set the location of the exceptions file')
@@ -60,5 +65,5 @@ if __name__=="__main__":
 
     mask = IN_MOVED_TO | IN_CREATE  # watched events -> add IN_DONT_FOLLOW to not follow symlinks, and IN_CREATE to watch created files
     wdd = wm.add_watch(working_dir, mask, rec=True, auto_add=True) #watch this directory, with mask(s), recursively
-    notifier.loop()#daemonize=True, pid_file=os.path.join(os.path.dirname(__file__), tvrenamrd.pid), force_kill=True, stdout='/opt/tvrenamr/stdout.txt')
+    notifier.loop()#daemonize=True, pid_file=os.path.join(os.path.dirname(__file__), tvrenamrd.pid), force_kill=True, stdout=os.path.join(os.path.dirname(__file__), stdout.txt))
 else: print 'This script is only designed to be run standalone'
