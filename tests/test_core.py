194348c318c8645a812bb5e99528229e969cce09
George Hickman <george@ghickman.co.uk>
2009-11-30 12:09:25 +0000

    moved file extraction tests into core testing module

--- tests/test_core.py
+++ tests/test_core.py
@@ -106,7 +106,8 @@ class TestCore(object):
         title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
         credentials['show'] = title['show']
         credentials['title'] = title['title']
-        assert_equals(self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], format='%n - %s%e - %t'), 'tests/data/working/Chuck - 108 - Chuck Versus the Truth.avi')
+        assert_equals(self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], 
+            extension=credentials['extension'], format='%n - %s%e - %t'), 'tests/data/working/Chuck - 108 - Chuck Versus the Truth.avi')
     
     def test_setting_an_episodes_format_as_season_episode_title_name(self):
         fn = 'chuck.s1e08.blah.HDTV.XViD.avi'
@@ -114,7 +115,8 @@ class TestCore(object):
         title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
         credentials['show'] = title['show']
         credentials['title'] = title['title']
-        assert_equals(self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], format='%s - %e - %t - %n'), 'tests/data/working/1 - 08 - Chuck Versus the Truth - Chuck.avi')
+        assert_equals(self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], 
+            extension=credentials['extension'], format='%s - %e - %t - %n'), 'tests/data/working/1 - 08 - Chuck Versus the Truth - Chuck.avi')
     
     def test_setting_an_episodes_format_as_title_episode_season_name(self):
         fn = 'chuck.s1e08.blah.HDTV.XViD.avi'
@@ -122,5 +124,71 @@ class TestCore(object):
         title = self.tv.retrieve_episode_name(show=credentials['show'], season=credentials['season'], episode=credentials['episode'])
         credentials['show'] = title['show']
         credentials['title'] = title['title']
-        assert_equals(self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], extension=credentials['extension'], format='%t - %e - %s - %n'), 'tests/data/working/Chuck Versus the Truth - 08 - 1 - Chuck.avi')
+        assert_equals(self.tv.build_path(show=credentials['show'], season=credentials['season'], episode=credentials['episode'], title=credentials['title'], 
+            extension=credentials['extension'], format='%t - %e - %s - %n'), 'tests/data/working/Chuck Versus the Truth - 08 - 1 - Chuck.avi')
     
+    def test_extracting_season_from_file_format_s0e00(self):
+        credentials = self.tv.extract_episode_details_from_file("chuck.s2e06.avi")
+        assert_equal(credentials['season'], '2')
+    
+    def test_extracting_season_from_file_format_s00e00(self):
+        credentials = self.tv.extract_episode_details_from_file("chuck.s20e05.avi")
+        assert_equal(credentials['season'], '20')
+    
+    def test_extracting_episode_from_file_format_s0e00(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.s2e05.avi')
+        assert_equal(credentials['episode'], '05')
+    
+    def test_extracting_episode_from_file_format_s00e00(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.s20e05')
+        assert_equal(credentials['episode'], '05')
+    
+    
+    def test_extracting_season_from_file_format_0x00(self):
+        credentials = self.tv.extract_episode_details_from_file("chuck.2x05.avi")
+        assert_equal(credentials['season'], '2')
+    
+    def test_extracting_season_from_file_format_00x00(self):
+        credentials = self.tv.extract_episode_details_from_file("chuck.20x05.avi")
+        assert_equal(credentials['season'], '20')
+    
+    def test_extracting_episode_from_file_format_0x00(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.2x05.avi')
+        assert_equal(credentials['episode'], '05')
+    
+    def test_extracting_episode_from_file_format_00x00(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.20x05')
+        assert_equal(credentials['episode'], '05')
+    
+    
+    def test_extracting_season_from_file_format_000(self):
+        credentials = self.tv.extract_episode_details_from_file("chuck.205.avi")
+        assert_equal(credentials['season'], '2')
+    
+    def test_extracting_season_from_file_format_0000(self):
+        credentials = self.tv.extract_episode_details_from_file("chuck.2005.avi")
+        assert_equal(credentials['season'], '20')
+    
+    def test_extracting_episode_from_file_format_000(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.205.avi')
+        assert_equal(credentials['episode'], '05')
+    
+    def test_extracting_episode_from_file_format_0000(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.2005')
+        assert_equal(credentials['episode'], '05')
+    
+    def test_extracting_season_with_custom_regular_expression_passing_in_season_and_episode_digit_lengths_from_file_format_000(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.025', user_regex='%n.%s{2}%e{1}')
+        assert_equal(credentials['season'], '02')
+    
+    def test_extracting_episode_with_custom_regular_expression_passing_in_season_and_episode_digit_lengths_from_file_format_000(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.025', user_regex='%n.%s{2}%e{1}')
+        assert_equal(credentials['episode'], '5')
+    
+    def test_extracting_season_with_custom_regular_expression_passing_in_season_digit_lengths_from_file_format_000(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.0250', user_regex='%n.%s{2}%e')
+        assert_equal(credentials['season'], '02')
+    
+    def test_extracting_season_with_custom_regular_expression_passing_in_episode_digit_lengths_from_file_format_000(self):
+        credentials = self.tv.extract_episode_details_from_file('chuck.025', user_regex='%n.%s%e{1}')
+        assert_equal(credentials['episode'], '5')
