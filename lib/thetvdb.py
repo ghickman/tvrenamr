1c3312971c28f3e440233c1354fb9f912eea8493
George Hickman <george@ghickman.co.uk>
2010-06-14 18:58:51 +0100

    updated libraries to expose get_title and get_show methods created instance variables to hold show name and episode names that are returned renamed some variables with more sane names

--- lib/thetvdb.py
+++ lib/thetvdb.py
@@ -11,13 +11,31 @@ url_series = 'GetSeries.php?seriesname='
 apikey = 'C4C424B4E9137AFD'
 
 class TheTvDb():
-    def __init__(self, show_name):
+    def __init__(self, show, season, episode):
         """
         :param show_name: The show name of the episode title to be retrieved.
         """
-        self.series = show_name
+        self.show = show
+        log.info('Looking up show: %s' % self.show)
+        self.season = season
+        self.episode = episode
         
-    def __get_series_id(self):
+        self.show_id, self.show = self.__get_show_id()
+        log.debug('Retrieved show id: %s' % self.show_id)
+        log.debug('Retrieved canonical show name: %s' % self.show)
+        self.title = self.__get_episode_name()
+        log.debug('Retrieved episode name: %s' % self.title)
+    
+    
+    def get_show(self):
+        return self.show
+    
+    
+    def get_title(self):
+        return self.title
+    
+    
+    def __get_show_id(self):
         """
         Retrieves the show ID of the show name passed in when the class is instantiated.
         
@@ -28,22 +46,23 @@ class TheTvDb():
         :returns: A show ID.
         :rtype: A string.
         """
-        url = url_base + url_series + urllib2.quote(self.series)
-        log.debug('Series url: '+url)
+        log.debug('Retrieving series id for %s' % self.show)
+        url = url_base + url_series + urllib2.quote(self.show)
+        log.debug('Series url: %s' % url)
         try: data = urllib2.urlopen(url).read()
         except urllib2.URLError: raise
         dom = ET.fromstring(data)
-        if dom is None: raise XMLEmptyException(log.name, self.series)
+        if dom is None: raise XMLEmptyException(log.name, self.show)
         log.debug('XML retrieved, searching for series')
         for name in dom.findall("Series"):
-            s = name.find("SeriesName").text
-            if s.lower() == self.series.lower():
-                self.series = s
-                log.debug('Series chosen: %s' % self.series)
-                return name.find('seriesid').text
-            else: raise ShowNotFoundException(log.name, self.series)
-        
-    def get_episode_name(self, season, episode):
+            show = name.find("SeriesName").text
+            if show.lower() == self.show.lower():
+                log.debug('Series chosen: %s' % show)
+                return name.find('seriesid').text, show
+            else: raise ShowNotFoundException(log.name, self.show)
+    
+    
+    def __get_episode_name(self):
         """
         Retrieves the episode title for the given episode from tvrage.com.
         
@@ -56,19 +75,14 @@ class TheTvDb():
         :returns: The series name and title. Series name is returned so that it is formatted correctly.
         :rtype: A dictionary whose keys are 'series' and 'title'.
         """
-        log.debug('Retrieving series id for %s' % self.series)
-        series_id = self.__get_series_id()
-        log.debug('Building episode url')
-        episode_url = url_base + apikey +'/series/'+ series_id +'/default/'+ str(int(season)) +'/'+ str(int(episode)) +'/en.xml'
-        log.debug(episode_url)
+        episode_url = url_base + apikey +'/series/'+ self.show_id +'/default/'+ str(int(self.season)) +'/'+ str(int(self.episode)) +'/en.xml'
+        log.debug('Episode URL: %s' % episode_url)
         log.debug('Attempting to retrieve episode name')
         try: f = urllib2.urlopen(episode_url)
-        except urllib2.URLError: raise EpisodeNotFoundException(log.name, self.series, season, episode)
+        except urllib2.URLError: raise EpisodeNotFoundException(log.name, self.show, self.season, self.episode)
         dom = ET.fromstring(f.read())
-        if dom is None: raise XMLEmptyException(log.name, self.series)
-        log.debug('Episode XML retrived for %s - %s%s' % (self.series, season, episode))
+        if dom is None: raise XMLEmptyException(log.name, self.show)
+        log.debug('Episode XML retrived for %s - %s%s' % (self.show, self.season, self.episode))
         
-        title = dom.find("Episode").find("EpisodeName").text
-        
-        log.info('Retrieved episode: %s' % title)
-        return {'show': self.series, 'title': title}
+        return dom.find("Episode").find("EpisodeName").text
+    
--- lib/tvrage.py
+++ lib/tvrage.py
@@ -11,13 +11,31 @@ url_ep = "http://services.tvrage.com/feeds/full_show_info.php?sid="
 
 
 class TvRage():
-    def __init__(self, show_name):
+    def __init__(self, show, season, episode):
         """
         :param show_name: The show name of the episode title to be retrieved.
         """
-        self.series = show_name
+        self.show = show
+        log.info('Looking up show: %s' % self.show)
+        self.season = str(int(season))
+        self.episode = episode
+        
+        self.show_id, self.show = self.__get_show_id()
+        log.debug('Retrieved show id: %s' % self.show_id)
+        log.debug('Retrieved canonical show name: %s' % self.show)
+        self.title = self.__get_episode_name()
+        log.debug('Retrieved episode name: %s' % self.title)
     
-    def __get_series_id(self):
+    
+    def get_show(self):
+        return self.show
+    
+    
+    def get_title(self):
+        return self.title
+    
+    
+    def __get_show_id(self):
         """
         Retrieves the show ID of the show name passed in when the class is instantiated.
         
@@ -28,21 +46,21 @@ class TvRage():
         :returns: A show ID.
         :rtype: A string.
         """
-        url = url_name + self.series.replace(' ', '%20')
+        url = url_name + self.show.replace(' ', '%20')
         try: data = urllib2.urlopen(url).read()
         except urllib2.URLError: raise
         dom = ET.fromstring(data)
-        if dom is None: raise XMLEmptyException(log.name, self.series)
+        if dom is None: raise XMLEmptyException(log.name, self.show)
         log.debug('XML retrieved, searching for series')
         for name in dom.findall('show'):
-            s = name.find('name').text
-            if s.lower() == self.series.lower():
-                self.series = s
-                log.debug('Series chosen %s' % self.series)
-                return name.find('showid').text
-            else: raise ShowNotFoundException(log.name, self.series)
+            show = name.find('name').text
+            if show.lower() == self.show.lower():
+                log.debug('Series chosen %s' % self.show)
+                return name.find('showid').text, show
+            else: raise ShowNotFoundException(log.name, self.show)
     
-    def get_episode_name(self, season, episode):
+    
+    def __get_episode_name(self):
         """
         Retrieves the episode title for the given episode from tvrage.com.
         
@@ -55,24 +73,23 @@ class TvRage():
         :returns: The series name and title. Series name is returned so that it is formatted correctly.
         :rtype: A dictionary whose keys are 'series' and 'title'.
         """
-        series_id = self.__get_series_id()
-        url = url_ep + series_id
+        url = url_ep + self.show_id
         try: data = urllib2.urlopen(url).read()
-        except urllib2.URLError: raise EpisodeNotFoundException(log.name, self.series)
+        except urllib2.URLError: raise EpisodeNotFoundException(log.name, self.show)
         dom = ET.fromstring(data)
-        if dom is None: raise XMLEmptyException(log.name, self.series)
+        if dom is None: raise XMLEmptyException(log.name, self.show)
         log.debug('XML retrieved, searching for episode')
         
         # In a single digit episode number add a zero
-        if len(episode) == 1 and episode[:1] != '0': episode = '0'+episode
+        if len(self.episode) == 1 and self.episode[:1] != '0': self.episode = '0' + self.episode
         
         title = None
         for s in dom.find('Episodelist'):
-            if s.get('no') == season:
+            if s.get('no') == self.season:
                 for e in s.findall('episode'):
-                    if e.find('seasonnum').text == episode:
+                    if e.find('seasonnum').text == self.episode:
                         title = e.find('title').text
                         log.info('Retrieved episode: %s' % title)
-        if title is not None: return {'show': self.series, 'title':title}
-        else: raise EpisodeNotFoundException(log.name, self.series, season, episode)
+        if title is not None: return title
+        else: raise EpisodeNotFoundException(log.name, self.show, self.season, self.episode)
     
