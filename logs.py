68d864a64b00558a180b6ef826cb29d2e600a3a4
George Hickman <george@ghickman.co.uk>
2010-06-23 22:38:04 +0100

    completely cleaned up logs into one method and cut down code

--- logs.py
+++ logs.py
@@ -5,80 +5,46 @@ import sys
 import logging
 import logging.handlers
 
-__version__ = '1.0'
-
-log = logging.getLogger('main')
-
-_logging_configured = False
-_mem_handler = None
-def initialize_logging(unit_test=False):
-    global _logging_configured, _mem_handler
-
-    if not _logging_configured:
-        if unit_test:
-            _logging_configured = True
-            return
-
-        # root logger
-        logger = logging.getLogger()
-
-        # time format is same format of strftime
-        log_format = ['%(asctime)-15s %(levelname)-8s %(name)-11s %(message)s', '%Y-%m-%d %H:%M']
-        formatter = logging.Formatter(*log_format)
-
-        _mem_handler = logging.handlers.MemoryHandler(1000*1000, 100)
-        _mem_handler.setFormatter(formatter)
-        logger.addHandler(_mem_handler)
-
-        logger.setLevel(logging.INFO)
-
-        _logging_configured = True
-
-_logging_started = False
-def start_logging(filename=None, level=logging.INFO, debug=False, quiet=False):
-    global _logging_configured, _mem_handler, _logging_started
-
-    if not _logging_started:
-        if debug:
-            hdlr = logging.StreamHandler()
-        else:
-            hdlr = logging.handlers.RotatingFileHandler(filename, maxBytes=1000*1024, backupCount=9)
-
-        hdlr.setFormatter(_mem_handler.formatter)
-
-        _mem_handler.setTarget(hdlr)
-
-        # root logger
-        logger = logging.getLogger()
-
-        logger.removeHandler(_mem_handler)
-        logger.addHandler(hdlr)
-
-        logger.setLevel(level)
-
-        if not debug and not quiet:
-            console = logging.StreamHandler()
-            console.setFormatter(hdlr.formatter)
-            logger.addHandler(console)
-
-            # flush memory handler to the console without
-            # destroying the buffer
-            if len(_mem_handler.buffer) > 0:
-                for record in _mem_handler.buffer:
-                    console.handle(record)
-
-        # flush what we have stored from the plugin initialization
-        _mem_handler.flush()
-
-        _logging_started = True
-        
-def flush_logging():
-    """Flushes memory logger to console"""
-    console = logging.StreamHandler()
-    console.setFormatter(_mem_handler.formatter)
+def start_logging(filename=None, debug=False, quiet=False):
+    # set defaults
+    format = ['%(message)s']
+    level = logging.INFO
+    file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=1000*1024, backupCount=9)
+    
+    # set debug options
+    if debug:
+        format = ['%(asctime)-15s %(levelname)-8s %(name)-11s %(message)s', '%Y-%m-%d %H:%M']
+        level = logging.DEBUG
+        file_handler = logging.StreamHandler()
+    
+    # root logger
     logger = logging.getLogger()
-    logger.addHandler(console)
-    if len(_mem_handler.buffer) > 0:
-        for record in _mem_handler.buffer:
-            console.handle(record)
-    _mem_handler.flush()
+    
+    # set the log format.
+    formatter = logging.Formatter(*format)
+    
+    mem_handler = logging.handlers.MemoryHandler(1000*1000, 100)
+    mem_handler.setFormatter(formatter)
+    logger.addHandler(mem_handler)
+    
+    file_handler.setFormatter(mem_handler.formatter)
+    
+    mem_handler.setTarget(file_handler)
+    
+    logger.removeHandler(mem_handler)
+    logger.addHandler(file_handler)
+    
+    logger.setLevel(level)
+    
+    if not debug and not quiet:
+        console = logging.StreamHandler()
+        console.setFormatter(file_handler.formatter)
+        logger.addHandler(console)
+        
+        # flush memory handler to the console without destroying the buffer
+        if len(mem_handler.buffer) > 0:
+            for record in mem_handler.buffer:
+                console.handle(record)
+    
+    # flush what we have stored from the module initialization
+    mem_handler.flush()
