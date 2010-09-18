bf982d4a5bd47a671a0d0b3c78c4b99924c1cff1
George Hickman <george@ghickman.co.uk>
2010-09-18 10:02:47 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'tvrenamr',
-    version = '2.1',
+    version = '2.1.1',
     description = 'Rename tv show files using online databases',
     author = 'George Hickman',
     author_email = 'george@ghickman.co.uk',
