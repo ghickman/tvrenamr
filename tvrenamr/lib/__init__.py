1037af9708ebf294233206a7b2e403d423cc2aed
George Hickman <george@ghickman.co.uk>
2010-10-03 15:47:34 +0100

    updated imports and pep8
    - pep8'd code
    - added empty episode name exception to import list
    
    note: importing empty episode name exception in this manner is not
    working currently. See lib/thetvdb.py

--- tvrenamr/lib/__init__.py
+++ tvrenamr/lib/__init__.py
@@ -2,13 +2,27 @@ import logging
 
 log = logging.getLogger('Lib Package')
 
-# import errors modules - this is horrible but the only way it will work currently.
+"""
+import the errors module - this feels like a horrible way to do it but it's
+the only way I could make it work currently.
+"""
+
 try:
     # when installed as a package
     log.debug('Attempting to import errors from tvrenamr package')
-    from tvrenamr.errors import EpisodeNotFoundException, NoNetworkConnectionException, ShowNotFoundException, XMLEmptyException
+    from tvrenamr.errors import EmptyEpisodeNameException, \
+                                EpisodeNotFoundException, \
+                                NoNetworkConnectionException, \
+                                ShowNotFoundException, \
+                                XMLEmptyException
 except ImportError:
     # when run locally
-    log.debug('Failed to import errors from package, falling back to relative import')
-    from . errors import EpisodeNotFoundException, NoNetworkConnectionException, ShowNotFoundException, XMLEmptyException
+    log.debug('Failed to import errors from package, falling back to relative \
+                import')
+    from . errors import EmptyEpisodeNameException, \
+                            EpisodeNotFoundException, \
+                            NoNetworkConnectionException, \
+                            ShowNotFoundException, \
+                            XMLEmptyException
+
 log.debug('Errors imported successfully')
