2af41fb995802c0f6ea702cc6fb34c5b200ee8e2
George Hickman <george@ghickman.co.uk>
2011-02-19 11:06:40 +0000

    Swapped credentials dictionary for an episode object
    - updated front end and core to take the new object instead of kwargs

--- tvrenamr/episode.py
+++ tvrenamr/episode.py
@@ -1,6 +1,7 @@
 class Episode(object):
-    
-    def __init__(self, show_name=None, season=None, episode=None, title=None, extension=None, format='%n - %s%e - %t.%x'):
+
+    def __init__(self, show_name=None, season=None, episode=None, title=None,
+            extension=None, format='%n - %s%e - %t.%x'):
         self.show_name = show_name
         self.title = title
         self.extension = extension
@@ -10,13 +11,17 @@ class Episode(object):
             self.season = season
         else:
             self.season = str(season)
-        
+
         if season is None:
             self.episode = episode
         else:
             self.episode = str(episode)
 
     def __getattr__(self, item):
+        """
+        Allow the retrieval of single digit episode numbers but return
+        it with a leading zero.
+        """
         if item is 'episode_2':
             return '0%s' % self.episode
         else:
@@ -29,16 +34,23 @@ class Episode(object):
         filename = self.format
         try:
             filename = filename.replace('%n', self.show_name)
+        except TypeError:
+            pass
+        try:
             filename = filename.replace('%s', self.season)
+        except TypeError:
+            pass
+        try:
             filename = filename.replace('%e', self.episode_2)
+        except TypeError:
+            pass
+        try:
             filename = filename.replace('%t', self.title)
+        except TypeError:
+            pass
+        try:
             filename = filename.replace('%x', self.extension)
-            return filename
-        except TypeError, e:
-            return '%s \n%s' % (filename, e)
+        except TypeError:
+            pass
+        return filename
 
-e = Episode('chuck', 2, 5)
-e.title = 'test'
-e.extension = 'avi'
-print e.episode
-print e
