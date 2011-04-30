c03b7891fc0de0edb7ae10e4066dc68064f7bf93
George Hickman <george@ghickman.co.uk>
2011-04-30 20:19:48 +0100

    Cleaned up the mocking code

--- tvrenamr/tests/urlopenmock.py
+++ tvrenamr/tests/urlopenmock.py
@@ -1,25 +1,30 @@
-import os
-import urllib2, urlparse
 from hashlib import md5
-from minimock import mock, Mock, restore
+from os.path import dirname, join
+from urllib2 import Request, urlopen
+
+from minimock import mock, restore
 
 def urlopen_stub(url, data=None, timeout=30):
-    """Mock urllib2.urlopen and return a local file handle or create file if not existent and then return it."""
+    """
+    Mock urllib2.urlopen and return a local file handle or create file if
+    not existent and then return it.
+    """
 
-    if isinstance(url, urllib2.Request):
+    if isinstance(url, Request):
         key = md5(url.get_full_url()).hexdigest()
     else:
         key = md5(url).hexdigest()
-    data_file = os.path.join(os.path.dirname(__file__), 'cache', "%s.xml" % key)
+    data_file = join(dirname(__file__), 'cache', '%s.xml' % key)
     try:
         f = open(data_file)
     except IOError:
-        restore()
-        data = urllib2.urlopen(url, data).read()
-        # re-mock it.
-        mock('urllib2.urlopen', returns_func=urlopen_stub, tracker=None)
-        f = open(data_file, "w").write(data)
-        f = open(data_file, "r")
+        restore() # restore normal function
+        data = urlopen(url, data).read()
+        mock('urlopen', returns_func=urlopen_stub, tracker=None) # re-mock it.
+        with open(data_file, 'w') as f:
+            f.write(data)
+        f = open(data_file, 'r')
     return f
 
-mock('urllib2.urlopen', returns_func=urlopen_stub, tracker=None)
+mock('urlopen', returns_func=urlopen_stub, tracker=None)
+
