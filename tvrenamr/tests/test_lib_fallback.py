41519c63f5051fc9efc9adf2a3f101c838548843
George Hickman <george@ghickman.co.uk>
2011-08-01 20:52:15 +0100

    Setup tests to cover the fallback library feature and invalid xml
    - Added some mocked_xml, including an invalid xml file so we can test
      locally faster.
    - Tests in fallback module are only looking at what happens with invalid
      xml for now since there are some oddities in the way mocked_xml()
      is returning file handles.

---
+++ tvrenamr/tests/test_lib_fallback.py
@@ -0,0 +1,31 @@
+from os.path import dirname, join
+from shutil import copytree, rmtree
+
+from mock import patch
+from nose.tools import assert_raises
+
+from tvrenamr.config import Config
+from tvrenamr.episode import Episode
+from tvrenamr.errors import NoMoreLibrariesException
+from tvrenamr.main import TvRenamr
+import urlopenmock
+
+class TestLibrariesFallback(object):
+    invalid_xml_file = join(dirname(__file__), 'mocked_xml', 'invalid.xml')
+    working = 'tests/data/working'
+
+    def setUp(self):
+        files = 'tests/data/files'
+        self.config = Config(join(dirname(__file__), 'config.yml'))
+        self.tv = TvRenamr(self.working, self.config)
+        copytree(files, self.working)
+
+    def tearDown(self):
+        rmtree(self.working)
+
+    @patch('urllib2.urlopen', new=urlopenmock.invalid_xml)
+    def test_rename_with_all_libraries_returning_invalid_xml(self):
+        episode = Episode()
+        episode.show, episode.season, episode.episode, episode.extension = self.tv.extract_details_from_file('chuck.s1e08.blah.HDTV.XViD.avi')
+        assert_raises(NoMoreLibrariesException, self.tv.retrieve_episode_name, episode)
+
--- tvrenamr/tests/urlopenmock.py
+++ tvrenamr/tests/urlopenmock.py
@@ -1,8 +1,33 @@
 from hashlib import md5
+from re import compile
 from os.path import dirname, join
 from urllib2 import Request, urlopen
 
 from minimock import mock, restore
+
+def invalid_xml(url, data=None, timeout=30):
+    """Mock urllib2.urlopen and return a local file handle to invalid.xml"""
+    return open(join(dirname(__file__), 'mocked_xml', 'invalid.xml'), 'r')
+
+def mocked_xml(url, data=None, timeout=30):
+    """Mock urllib2.urlopen and return """
+    def return_file(lib, file_type):
+        files = {'episode': '108', 'series': 'chuck'}
+        mocked_xml = join(dirname(__file__), 'mocked_xml', lib, files[file_type] + '.xml')
+        return open(mocked_xml, 'r')
+
+    # workout the library we're working with
+    m = compile('http:\/\/(www\.)?([\w]+)\.com').match(url)
+    lib = m.group(2)
+    if lib == 'thetvdb':
+        if str.find(url, 'GetSeries') > 0:
+            return return_file(lib, 'series')
+
+        if str.find(url, 'en.xml') > 0:
+            return_file(lib, 'episode')
+
+    if lib == 'tvrage':
+        return open(join(dirname(__file__), 'invalid.xml'), 'r')
 
 def urlopen_stub(url, data=None, timeout=30):
     """
@@ -16,7 +41,7 @@ def urlopen_stub(url, data=None, timeout=30):
         key = md5(url).hexdigest()
     data_file = join(dirname(__file__), 'cache', '%s.xml' % key)
     try:
-        f = open(data_file)
+        f = open(data_file, 'r')
     except IOError:
         restore() # restore normal function
         data = urlopen(url, data).read()
