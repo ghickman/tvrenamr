c0e8099c9790c2c070fd5fc97fe11a0798b1f928
George Hickman <george@ghickman.co.uk>
2012-02-06 23:14:10 +0000

    Add a long description

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+from os.path import dirname, join
 from setuptools import setup, find_packages
 
 from tvrenamr import get_version
+
+
+def fread(fn):
+    with open(join(dirname(__file__), fn), 'r') as f:
+        return f.read()
 
 setup(
     name = 'tvrenamr',
     version = get_version(),
     description = 'Rename tv show files using online databases',
+    long_description = fread('README.md'),
     author = 'George Hickman',
     author_email = 'george@ghickman.co.uk',
     url = 'http://tvrenamr.info',
