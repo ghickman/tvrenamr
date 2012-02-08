088971f885c13beed63bfa71e7f7c157897510b5
George Hickman <george@ghickman.co.uk>
2012-02-08 22:30:18 +0000

    Dynamically build the test files

--- tvrenamr/tests/base.py
+++ tvrenamr/tests/base.py
@@ -1,4 +1,5 @@
-from os.path import dirname, join
+from os import makedirs
+from os.path import abspath, dirname, exists, join
 from shutil import copytree, rmtree
 
 from tvrenamr.config import Config
@@ -10,11 +11,20 @@ class BaseTest(object):
     files = 'tests/files'
     working = 'tests/data/working'
 
-    def setUp(self):
-        self.config = Config(join(dirname(__file__), 'config.yml'))
+    def __init__(self):
+        self.path = abspath(dirname(__file__))
+
+    def setup(self):
+        if not exists(self.files):
+            makedirs(self.files)
+        with open(join(self.path, 'file_list'), 'r') as f:
+            for fn in f.readlines():
+                with open(abspath(join(self.files, fn.strip())), 'w') as f:
+                    f.write('')
+        self.config = Config(join(self.path, 'config.yml'))
         self.tv = TvRenamr(self.working, self.config)
         copytree(self.files, self.working)
 
-    def tearDown(self):
+    def teardown(self):
         rmtree(self.working)
 
