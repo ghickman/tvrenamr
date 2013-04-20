4f2df39d909632e0d7a25c739daf8f2c1fa52cbb
George Hickman <george@ghickman.co.uk>
2013-04-20 22:08:27 +0100

    Use prints and str.format

--- tvrenamr/config.py
+++ tvrenamr/config.py
@@ -57,40 +57,38 @@ class Config(object):
             return safe_load(file(config))
         except Exception as e:
             self.log.critical(e)
-            print ''
-            print '-' * 79
-            print ' Malformed configuration file, common reasons:'
-            print '-' * 79
-            print ''
-            print ' o Indentation error'
-            print ' o Missing : from end of the line'
-            print ' o Non ASCII characters (use UTF8)'
-            print " o If text contains any of :[]{}% characters it must be \
-                    single-quoted ('')\n"
+            print('')
+            print('-'*79)
+            print(' Malformed configuration file, common reasons:')
+            print('-' * 79)
+            print('')
+            print(' o Indentation error')
+            print(' o Missing : from end of the line')
+            print(' o Non ASCII characters (use UTF8)')
+            print(" o If text contains any of :[]{}% characters it must be \
+                    single-quoted ('')\n")
             lines = 0
             if e.problem is not None:
-                print ' Reason: %s\n' % e.problem
+                print(' Reason: %s\n'.format(e.problem))
                 if e.problem == 'mapping values are not allowed here':
-                    print ' ----> MOST LIKELY REASON: Missing : from end of \
-                            the line!'
-                    print ''
+                    print(' ----> MOST LIKELY REASON: Missing : from end of \
+                            the line!')
+                    print('')
             if e.context_mark is not None:
-                print ' Check configuration near line %s, column %s' % \
-                        (e.context_mark.line, e.context_mark.column)
+                args = (e.context_mark.line, e.context_mark.column)
+                print(' Check configuration near line {}, column {}'.format(*args))
                 lines += 1
             if e.problem_mark is not None:
-                print ' Check configuration near line %s, column %s' % \
-                        (e.problem_mark.line, e.problem_mark.column)
+                args = (e.problem_mark.line, e.problem_mark.column)
+                print(' Check configuration near line {}, column {}'.format(*args))
                 lines += 1
             if lines:
-                print ''
+                print('')
             if lines == 1:
-                print ' Fault is almost always in this or previous line\n'
+                print(' Fault is almost always in this or previous line\n')
             if lines == 2:
-                print ' Fault is almost always in one of these lines or \
-                        previous ones\n'
-            # if self.options.debug:
-                # raise
+                print(' Fault is almost always in one of these lines or \
+                        previous ones\n')
             sys.exit(1)
 
     def _get_defaults(self):
@@ -103,4 +101,3 @@ class Config(object):
             For an example see: https://gist.github.com/586062
             """
             raise NameError(message)
-
