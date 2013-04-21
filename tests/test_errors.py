c666a08dc870313fe9b38824af01f4e6d8562494
George Hickman <george@ghickman.co.uk>
2013-04-21 11:15:19 +0100

    Wrap file exists test in file opening context manager
    IIRC when using open as a context manager the file handle is closed when
    you leave the scope of the context manager. While the file should still
    exist I'm not sure how Travis' architecture deals with this. I should
    probably try a temp file out too...

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -22,13 +22,12 @@ class TestExceptionsAreRaised(BaseTest):
         assert_raises(NoMoreLibrariesException, self.tv.retrieve_episode_name, episode)
 
     def test_episode_already_exists_raise_exception(self):
-        with open(join(self.files, 'Chuck - 205 - Chuck Versus Tom Sawyer.avi'), 'w'):
-            pass
         fn = 'chuck.s02e05.avi'
         episode = Episode(**self.tv.extract_details_from_file(fn))
         episode.title = self.tv.retrieve_episode_name(episode)
         path = self.tv.build_path(episode, organise=False)
-        assert_raises(EpisodeAlreadyExistsInDirectoryException, self.tv.rename, fn, path)
+        with open(join(self.files, 'Chuck - 205 - Chuck Versus Tom Sawyer.avi'), 'w'):
+            assert_raises(EpisodeAlreadyExistsInDirectoryException, self.tv.rename, fn, path)
 
     def test_custom_syntax_snippets_missing_raises_exception(self):
         assert_raises(IncorrectCustomRegularExpressionSyntaxException,
