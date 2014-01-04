09de1d2975659381894d4297413800f252cb3158
George Hickman <george@ghickman.co.uk>
2014-01-04 12:53:21 +0000

    Tidy up setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
-import multiprocessing  # stop tests breaking tox
+import multiprocessing  # noqa # stop tests breaking tox
 from setuptools import setup
 
 import tvrenamr
 
 
-requires = ('pyyaml', 'requests')
-packages = ('tvrenamr',)
-
+requires = ['pyyaml', 'requests']
 setup_requires = ('minimock', 'mock', 'nose', 'pyyaml')
 
 setup(
-    name = tvrenamr.__title__,
-    version = tvrenamr.__version__,
-    description = 'Rename tv show files using online databases',
-    long_description = open('README.rst').read() + '\n\n' +
-                       open('CHANGELOG.rst').read(),
-    author = tvrenamr.__author__,
-    author_email = 'george@ghickman.co.uk',
-    url = 'http://tvrenamr.info',
-    license = 'MIT',
-    packages = packages,
-    entry_points = {'console_scripts': ['tvr = tvrenamr.frontend:run']},
-    classifiers = (
+    name=tvrenamr.__title__,
+    version=tvrenamr.__version__,
+    description='Rename tv show files using online databases',
+    long_description=open('README.rst').read() + '\n\n' +
+    open('CHANGELOG.rst').read(),
+    author=tvrenamr.__author__,
+    author_email='george@ghickman.co.uk',
+    url='http://tvrenamr.info',
+    license='MIT',
+    packages=['tvrenamr'],
+    entry_points={'console_scripts': ['tvr=tvrenamr.frontend:run']},
+    classifiers=(
         'Development Status :: 6 - Mature',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
@@ -34,8 +32,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Utilities',
     ),
-    install_requires = requires,
+    install_requires=requires,
     setup_requires=setup_requires,
     test_suite='nose.collector',
 )
-
