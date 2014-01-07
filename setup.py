33cbc698a4604df334ef7128a5ff137e034b00d5
George Hickman <george@ghickman.co.uk>
2014-01-07 17:16:09 +0000

    *grumble*

--- setup.py
+++ setup.py
@@ -19,19 +19,19 @@ setup(
     license='MIT',
     packages=['tvrenamr'],
     entry_points={'console_scripts': ['tvr=tvrenamr.frontend:run']},
-    classifiers=(
+    classifiers=[
         'Development Status :: 6 - Mature',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: English'
+        'Natural Language :: English',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Topic :: Utilities',
-    ),
+    ],
     install_requires=requires,
     setup_requires=setup_requires,
     test_suite='nose.collector',
