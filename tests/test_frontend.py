1f8ca69e673bf8fb00bd4c8f9a8036652f2d2483
George Hickman <george@ghickman.co.uk>
2014-03-15 21:16:34 -0700

    Fix import paths in frontend tests

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -4,39 +4,35 @@ import random
 from nose.tools import assert_true
 
 from .base import BaseTest
-from tvrenamr.frontend import build_file_list, FrontEnd
+from tvrenamr import frontend
 
 
 class TestFrontEnd(BaseTest):
     def setup(self):
         super(TestFrontEnd, self).setup()
-        self.frontend = FrontEnd()
-        self.frontend.get_config(os.path.join(self.path, 'config.yml'))
+        self.config = frontend.get_config()
 
     def random_files(self, files):
         possible_files = os.listdir(self.files)
         return [os.path.join(files, random.choice(possible_files)) for choice in range(3)]
 
-    def test_config_variable_exists(self):
-        assert_true(hasattr(self.frontend, 'config'))
-
     def test_build_file_list_from_a_folder_path(self):
-        file_list = build_file_list([self.files])
+        file_list = frontend.build_file_list([self.files])
         for fn in os.listdir(self.files):
             if os.path.isdir(fn):
                 assert_true(os.path.join(self.files, fn) in file_list)
 
     def test_passing_current_dir_makes_file_list_a_list(self):
-        assert_true(isinstance(build_file_list([self.files]), list))
+        assert_true(isinstance(frontend.build_file_list([self.files]), list))
 
     def test_build_file_list_from_multiple_files(self):
         files = self.random_files(self.files)
-        file_list = build_file_list(files)
+        file_list = frontend.build_file_list(files)
         assert_true(all(fn in file_list for fn in files))
 
     def test_build_file_list_from_folders_and_files(self):
         files = self.random_files(self.files) + [self.subfolder]
-        file_list = build_file_list(files)
+        file_list = frontend.build_file_list(files)
 
         def final_list():
             files = []
@@ -49,7 +45,7 @@ class TestFrontEnd(BaseTest):
 
     def test_build_file_list_from_single_file(self):
         fn = os.path.join(self.files, random.choice(os.listdir(self.files)))
-        file_list = build_file_list([fn])
+        file_list = frontend.build_file_list([fn])
         assert_true(fn in file_list)
 
     def test_setting_recursive_adds_all_files_below_the_folder(self):
@@ -63,12 +59,12 @@ class TestFrontEnd(BaseTest):
         build_folder('herp')
         build_folder('herp/derp')
         build_folder('herp/derp/test')
-        file_list = build_file_list([self.files], recursive=True)
+        file_list = frontend.build_file_list([self.files], recursive=True)
         for root, dirs, files in os.walk(self.files):
             for fn in files:
                 assert_true(os.path.join(root, fn) in file_list)
 
     def test_ignoring_files(self):
         ignore = self.random_files(self.files)
-        file_list = build_file_list([self.files], ignore_filelist=ignore)
+        file_list = frontend.build_file_list([self.files], ignore_filelist=ignore)
         assert_true(all(not fn in file_list for fn in ignore))
